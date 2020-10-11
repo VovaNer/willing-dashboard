693bdec44df5ab4b08d3e61481f3152bf3c3dabf
VovaNer <VovaNer@users.noreply.github.com>
2020-10-12 00:21:43 +0400

    screen header done

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,11 @@ const screenCategory = document.querySelector('.screen__option-category'),
     optionSelectCategory = document.querySelector('.option__select-category'),
     optionSelectFilter = document.querySelector('.option__select-filter'),
     categoryDropdown = document.querySelector('.option__dropdown-category'),
-    filterDropdown = document.querySelector('.option__dropdown-filter');
+    filterDropdown = document.querySelector('.option__dropdown-filter'),
+    searchBtn = document.querySelector('.screen_controls-search');
 
 
-screenCategory.addEventListener('click', (event) => {
+document.addEventListener('click', (event) => {
     if (screenCategory.contains(event.target)) {
         if (!optionSelectCategory.classList.contains('option__select-active')) {
             optionSelectCategory.style.borderRadius = '15px 15px 0 0';
@@ -19,12 +20,7 @@ screenCategory.addEventListener('click', (event) => {
             categoryDropdown.classList.remove('option__select-active');
             categoryDropdown.style.opacity = '0';
         }
-    }    
-});
-
-screenFilter.addEventListener('click', (event) => {
-    console.log('yes2');
-    if (screenFilter.contains(event.target)) {
+    } else if (screenFilter.contains(event.target)) {
         if (!optionSelectFilter.classList.contains('option__select-active')) {
             optionSelectFilter.style.borderRadius = '15px 15px 0 0';
             optionSelectFilter.classList.add('option__select-active');
@@ -36,5 +32,28 @@ screenFilter.addEventListener('click', (event) => {
             filterDropdown.classList.remove('option__select-active');
             filterDropdown.style.opacity = '0';
         }
-    }    
+    } else if (searchBtn.contains(event.target)) {
+        if (!searchBtn.classList.contains('search-active')) {
+            searchBtn.classList.add('search-active');
+        }
+    } else {
+        console.log(event.target);
+        optionSelectCategory.style.borderRadius = '15px';
+        optionSelectCategory.classList.remove('option__select-active');
+        categoryDropdown.classList.remove('option__select-active');
+        categoryDropdown.style.opacity = '0';
+
+        optionSelectFilter.style.borderRadius = '15px';
+        optionSelectFilter.classList.remove('option__select-active');
+        filterDropdown.classList.remove('option__select-active');
+        filterDropdown.style.opacity = '0';
+
+        searchBtn.classList.remove('search-active');
+    }   
 });
+
+// searchBtn.addEventListener('click', () => {
+//     if (!searchBtn.classList.contains('search-active')) {
+//         searchBtn.classList.add('search-active');
+//     }
+// });
